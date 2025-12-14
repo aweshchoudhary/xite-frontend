dc3ccb5bf8315598793ea54efb406f77c6a36bd4
Awesh Choudhary <aweshchoudhary7@gmail.com>
2025-12-14 18:57:58 +0530

    Remove package-lock.json and update package.json to include @dnd-kit/core and @dnd-kit/sortable dependencies. Refactor database connection handling by removing PostgreSQL pool and adjusting Prisma client initialization for improved environment variable management.

--- modules/common/database/index.ts
+++ modules/common/database/index.ts
@@ -1,4 +1,3 @@
 import { prisma, Prisma } from "./prisma/connection";
-export * from "./prisma/generated/prisma";
 
 export { prisma as primaryDB, type Prisma as PrimaryDB };
--- modules/common/database/prisma/connection.ts
+++ modules/common/database/prisma/connection.ts
@@ -1,20 +1,10 @@
-import { PrismaClient, Prisma } from "./generated/prisma/client";
+import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
+import { PrismaClient, Prisma } from "./generated/prisma";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const connectionString = `${process.env.DATABASE_URL}`;
 
-const connectionString = process.env.DATABASE_URL;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
-if (!connectionString) {
-  throw new Error("DATABASE_URL environment variable is not set");
-}
-
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-
-const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export { type Prisma, prisma };
+export { prisma, type Prisma };
