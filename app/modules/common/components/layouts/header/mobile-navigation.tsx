9449fc2fd146be3be565f3dffe0fda8b739082a9
Awesh Choudhary <aweshchoudhary7@gmail.com>
2026-01-05 20:11:25 +0530

    Refactor mobile navigation to filter links based on user permissions. Replaced previous link rendering logic with a memoized approach to enhance performance and ensure only accessible links are displayed.

--- app/modules/common/components/layouts/header/mobile-navigation.tsx
+++ app/modules/common/components/layouts/header/mobile-navigation.tsx
@@ -3,43 +3,44 @@ import Link from "next/link";
 import { links } from "../../global/sidebar";
 import { usePathname } from "next/navigation";
 import { cn } from "@/modules/common/lib/utils";
-import { useHasPermission } from "@/modules/common/authentication/access-control/lib";
-import { Link as LinkType } from "../../global/sidebar/links";
+import { hasPermission } from "@/modules/common/authentication/access-control/lib/check-permission";
+import { useAuth } from "@/modules/common/authentication/firebase/use-auth-hook";
+import { useMemo } from "react";
 
 export default function MobileNavigation() {
-  return (
-    <div className="flex items-center justify-center px-5 bg-primary text-primary-foreground">
-      <ul className="flex items-center flex-wrap justify-center gap-4 py-3">
-        {links.map((link) => (
-          <LinkItem key={link.url} link={link} />
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-const LinkItem = ({ link }: { link: LinkType }) => {
   const pathname = usePathname();
+  const { roles } = useAuth();
+
+  // Filter links based on permissions once, using useMemo to prevent recalculation
+  const filteredLinks = useMemo(() => {
+    return links.filter((link) => hasPermission(roles, link.resource, "read"));
+  }, [roles]);
+
   const isActive = (url: string, exact: boolean = false) => {
     if (exact) {
       return pathname === url;
     }
     return pathname.startsWith(url);
   };
-  const hasPermission = useHasPermission(link.resource, "read");
-  if (hasPermission)
-    return (
-      <li key={link.url}>
-        <Link
-          className={cn(
-            "px-3 py-1.5 rounded-md",
-            isActive(link.url, link.exact) &&
-              "bg-primary-foreground text-foreground"
-          )}
-          href={link.url}
-        >
-          {link.title}
-        </Link>
-      </li>
-    );
-};
+
+  return (
+    <div className="flex items-center justify-center px-5 bg-primary text-primary-foreground">
+      <ul className="flex items-center flex-wrap justify-center gap-4 py-3">
+        {filteredLinks.map((link) => (
+          <li key={link.url}>
+            <Link
+              className={cn(
+                "px-3 py-1.5 rounded-md",
+                isActive(link.url, link.exact) &&
+                  "bg-primary-foreground text-foreground"
+              )}
+              href={link.url}
+            >
+              {link.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
