ec30c77ca6821924a6d8b0f4ba522b6f052e0376
Awesh Choudhary <aweshchoudhary7@gmail.com>
2026-01-26 16:35:20 +0530

    Refactor createProgramAction to streamline database transaction logic and update schema for curriculum and faculty sections. Enhance curriculum section creation with new naming conventions and include overview description in cohort overview section. This improves data handling and aligns with recent schema updates.

--- app/modules/program/components/forms/create-b2b/schema.ts
+++ app/modules/program/components/forms/create-b2b/schema.ts
@@ -1,56 +1,60 @@
 import { z } from "zod";
 
 export const moduleSchema = z.object({
-    title: z.string().min(1).max(255),
-    overview: z.string().optional().nullable(),
-    position: z.number(),
-    objectives: z.array(
-      z.object({
-        description: z.string().optional().nullable(),
-        position: z.number(),
-      })
-    ),
-    sessions: z.array(
-      z.object({
-        title: z.string().min(1).max(255),
-        position: z.number(),
-        overview: z.string().optional().nullable(),
-        topic_id: z.string().optional().nullable(),
-        sub_topic_id: z.string().optional().nullable(),
-        objectives: z.array(
-          z.object({
-            description: z.string().optional().nullable(),
-            position: z.number(),
-          })
-        ),
-      })
-    ),
-  });
-  
+  title: z.string().min(1).max(255),
+  overview: z.string().optional().nullable(),
+  position: z.number(),
+  objectives: z.array(
+    z.object({
+      description: z.string().optional().nullable(),
+      position: z.number(),
+    }),
+  ),
+  sessions: z.array(
+    z.object({
+      title: z.string().min(1).max(255),
+      position: z.number(),
+      overview: z.string().optional().nullable(),
+      topic_id: z.string().optional().nullable(),
+      sub_topic_id: z.string().optional().nullable(),
+      objectives: z.array(
+        z.object({
+          description: z.string().optional().nullable(),
+          position: z.number(),
+        }),
+      ),
+    }),
+  ),
+});
+
 export const curriculumSchema = z.object({
- items: z.array(moduleSchema).min(1),
+  items: z.array(moduleSchema).min(1),
 });
 
 export const createSchema = z.object({
- program_name: z.string().min(1).max(255),
- program_short_name: z.string().min(1).max(255),
- program_key: z.string().min(1).max(255),
- cohort_start_date: z.date(),
- cohort_end_date: z.date(),
- cohort_format: z.string(),
- cohort_duration: z.string(),
- cohort_location: z.string(),
- overview_description: z.string(),
- enterprise_id: z.uuid(),
- academic_partner_id: z.uuid(),
- proposal: z.string().optional(),
- tags: z.array(z.string()).optional(),
-//  assigned_to: z.uuid(),
- faculties: z.array(z.object({
-  facultyId: z.uuid(),
-  position: z.number(),
-})).min(1),
- curriculum: curriculumSchema,
+  program_name: z.string().min(1).max(255),
+  program_short_name: z.string().min(1).max(255),
+  program_key: z.string().min(1).max(255),
+  cohort_start_date: z.date(),
+  cohort_end_date: z.date(),
+  cohort_format: z.string(),
+  cohort_duration: z.string(),
+  cohort_location: z.string(),
+  overview_description: z.string(),
+  enterprise_id: z.uuid(),
+  academic_partner_id: z.uuid(),
+  proposal: z.string(),
+  tags: z.array(z.string()).optional(),
+  //  assigned_to: z.uuid(),
+  faculties: z
+    .array(
+      z.object({
+        facultyId: z.uuid(),
+        position: z.number(),
+      }),
+    )
+    .min(1),
+  curriculum: curriculumSchema,
 });
 
 // Types
